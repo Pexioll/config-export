efe84fb83690fc10ded6f465d6d7b14bffa03611
Paweł Suchecki <115819984+Pexioll@users.noreply.github.com>
2024-04-02 14:28:59 +0200

    Delete .config-export directory

--- .config-export/i3/autostart.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-xrandr --output DVI-D-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off
-bash feh --bg-fill '/home/pawel/Pictures/bg.jpg'
-## Launch Polybar
